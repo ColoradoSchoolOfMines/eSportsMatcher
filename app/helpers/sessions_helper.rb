2a59ebe09125355c204285a70bdb96fb96d86c99
Travis Johnson <cjohnso0@mymail.mines.edu>
2016-09-15 17:53:21 -0600

    Finish section 9.1 of railstutorial to provide persistent user sessions 	e.g. user session doesn't end when the browser closes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,15 +4,34 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Returns the current logged-in user (if any)
   def current_user
-    # find_by will return nil when no record is found
-    # so find_by is more appropriate than find in this case
-    # as find will return an error when no record is found
-    # also, this `||=` operator essentially enforces that
-    # we don't do database lookups if the current_user is already
-    # known by the session
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      # this `||=` operator essentially enforces that
+      # we don't do database lookups if the current_user is already
+      # known by the session
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)  # find_by will return nil when no record is found
+      # so find_by is more appropriate than find in this case
+      # as find will return an error when no record is found
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -21,6 +40,7 @@ module SessionsHelper
   end
 
   def log_out
+    forget current_user
     session.delete(:user_id)
     @current_user = nil
   end
