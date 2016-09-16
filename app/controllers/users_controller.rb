01f72d0fdc5fc047962b6f69324cb00e86db716c
Travis Johnson <cjohnso0@mymail.mines.edu>
2016-09-16 10:07:38 -0600

    Admins can now delete users from the user/index view Test suite ensures non-admins will not be able to delete user accounts Finish chapter 10 of railstutorial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   # before controller does edit or update actions - check for logged_in_user
+  before_action :correct_user, only: [:edit, :update]
+  # correct_user must be logged in to do edit/update actions
+  before_action :admin_user, only: :destroy
+  # only admin users can destroy user accounts (through the destroy action of the controller)
   
   def index
     @users = User.paginate(page: params[:page])
@@ -40,6 +43,13 @@ class UsersController < ApplicationController
      end
    end
 
+   def destroy
+     @user = User.find(params[:id])
+     @user.destroy
+     flash[:success] = "User #{@user.name} deleted"
+     redirect_to users_url
+   end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
@@ -61,5 +71,9 @@ class UsersController < ApplicationController
     def correct_user 
       @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
