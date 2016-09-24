8304c7e8dba7c12ec58a9583408b3d072b95feca
Travis Johnson <cjohnso0@mymail.mines.edu>
2016-09-24 11:31:51 -0600

    Ran "rails generate model Summoner riot_id:integer name:string profileIconId:integer summonerLevel:integer"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916230041) do
+ActiveRecord::Schema.define(version: 20160924173114) do
+
+  create_table "summoners", force: :cascade do |t|
+    t.integer  "riot_id"
+    t.string   "name"
+    t.integer  "profileIconId"
+    t.integer  "summonerLevel"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["user_id"], name: "index_summoners_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
